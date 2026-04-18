52442483fdb3993c7754640acccbb4f247244274
amindell11 <amindell11@gmail.com>
2026-04-17 22:46:35 -0700

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,11 @@
 // Security is enforced by Firestore rules (see FIREBASE_SETUP.md).
 
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCGOxYUT6mrZd25l7JH1w5MIFWfQ6z2OJQ",
+  authDomain: "japan-trip-a8904.firebaseapp.com",
+  projectId: "japan-trip-a8904",
+  storageBucket: "japan-trip-a8904.firebasestorage.app",
+  messagingSenderId: "415229580579",
+  appId: "1:415229580579:web:03dddb0586fbb06784e896",
+  measurementId: "G-VKQCYV712Z"
 };
